ba94bf8c4b6e8965d0d4f7da903426cf1f26d432
Shawntez Malone <69469670+shawntez32@users.noreply.github.com>
2022-04-17 12:34:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
 const express = require('express');
 const https =require('https');
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get('/',function(req,res){
-  const url =('https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid=b91aaa37f11b9138fcd81d8268a62a10')
-  https.get(url, function(response){
-    console.log(response.statusCode);
+app.use(bodyParser.urlencoded({extended: true}));
 
-    response.on('data', function(data){
-      const weatherData = JSON.parse(data);
-      const object = {
-        name: 'Shawntez',
-        favouriteFood: 'Pizza'
-      }
-      console.log(JSON.stringify(data));
-  });
-  res.send('Server is up and running')
+app.get('/',function(req,res){
+  res.sendFile(__dirname + "/index.html");
+
 });
+
+app.post('/', function(req,res){
+  const query =  req.body.cityName
+  const apiKey = 'b91aaa37f11b9138fcd81d8268a62a10';
+  const unit = 'imperial'
+  const url ='https://api.openweathermap.org/data/2.5/weather?q='+ query +'&appid=' + apiKey + '&units=' + unit;
+  https.get(url, function(response){
+   console.log(response.statusCode);
+
+   response.on('data', function(data){
+     const weatherData = JSON.parse(data);
+     const temp = weatherData.main.temp
+      const weatherDescription = weatherData.weather[0].description
+      const icon = weatherData.weather[0].icon
+      const imageUrl = "http://openweathermap.org/img/wn/" + icon + "+@2x.png"
+      res.write("<h1>The weather is currently " + temp + "<h1>");
+      res.write('<p>' + icon + "</p>" )
+      res.write("<img src=" + imageUrl +">");
+      res.send()
+  })
+  })
+
 });
+
+
+
 app.listen(1111, function(){
   console.log('Server on Port 1111');
 });
